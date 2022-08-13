80c7dfdf76843afbc06e23be328270a77cb26ee8
Spencer <hello@spencercoleman.dev>
2022-08-13 02:41:29 -0700

    Implement react-router and initial routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,15 @@
-function App() {
-    return <div></div>;
-}
+import { Routes, Route } from 'react-router-dom';
+
+const App = () => {
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<h1>Home</h1>} />
+				<Route path="parties" element={<h1>Parties</h1>}></Route>
+				<Route path="parties/:partyId" element={<h1>Party</h1>} />
+			</Routes>
+		</>
+	);
+};
 
 export default App;
