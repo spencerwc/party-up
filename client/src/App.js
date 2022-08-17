4f991329f671f642090f684e4729031eb856ada3
spencercoleman <spencer@edc.vc>
2022-08-16 22:12:14 -0700

    Implement auth context and hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
 import Layout from './layouts/Layout';
 import Signup from './pages/Signup';
 
@@ -14,12 +15,20 @@ const ScrollToTop = () => {
 };
 
 const App = () => {
+    const { user } = useAuthContext();
+
     return (
         <>
             <ScrollToTop />
             <Routes>
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/login" element={<h1>Login</h1>} />
+                <Route
+                    path="/signup"
+                    element={!user ? <Signup /> : <Navigate to="/" />}
+                />
+                <Route
+                    path="/login"
+                    element={!user ? <h1>Login</h1> : <Navigate to="/" />}
+                />
                 <Route path="*" element={<Layout />} />
             </Routes>
         </>
