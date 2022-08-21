1705b8bfc09d262516fc0e24b67b6f608e3a87c0
Spencer <hello@spencercoleman.dev>
2022-08-21 02:29:12 -0700

    Adjust user population on getParty to select username and avatar

--- server/models/partyModel.js
+++ server/models/partyModel.js
@@ -3,35 +3,36 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const partySchema = new Schema({
-    date: {
-        type: Date,
-        required: true,
-    },
-    details: {
-        type: String,
-        required: true,
-    },
-    leader: {
-        type: Schema.Types.ObjectId,
-        // TODO: Make required
-    },
-    lookingFor: {
-        type: Number,
-        requireD: true,
-        min: 1,
-    },
-    members: {
-        type: [Schema.Types.ObjectId],
-        ref: 'User',
-        required: true,
-    },
-    messages: {
-        type: [Schema.Types.ObjectId],
-    },
-    name: {
-        type: String,
-        required: true,
-    },
+	date: {
+		type: Date,
+		required: true,
+	},
+	details: {
+		type: String,
+		required: true,
+	},
+	leader: {
+		type: Schema.Types.ObjectId,
+		ref: 'User',
+		required: true,
+	},
+	lookingFor: {
+		type: Number,
+		requireD: true,
+		min: 1,
+	},
+	members: {
+		type: [Schema.Types.ObjectId],
+		ref: 'User',
+		required: true,
+	},
+	messages: {
+		type: [Schema.Types.ObjectId],
+	},
+	name: {
+		type: String,
+		required: true,
+	},
 });
 
 module.exports = mongoose.model('Party', partySchema);
