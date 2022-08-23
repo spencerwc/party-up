94e2b2dadb4479aa50ddfbcae6df10670ba74e73
spencercoleman <spencer@edc.vc>
2022-08-23 00:23:34 -0700

    Add initial fields and validation to CreatePartyForm

--- client/src/components/CreatePartyForm.js
+++ client/src/components/CreatePartyForm.js
@@ -1,5 +1,75 @@
+import { useForm } from '@mantine/form';
+import { DatePicker } from '@mantine/dates';
+import { NumberInput, TextInput, Textarea, Button } from '@mantine/core';
+
 const CreatePartyForm = () => {
-    return <form></form>;
+    const form = useForm({
+        initialValues: {
+            name: '',
+            date: new Date(Date.now()),
+            lookingFor: 1,
+            details: '',
+        },
+
+        validate: {
+            name: (value) =>
+                value.length < 2 ? 'Name must have at least 2 letters' : null,
+            date: (value) =>
+                value.length <= 0 ? 'Date must be selected' : null,
+            lookingFor: (value) =>
+                value < 1 || value > 100
+                    ? 'Looking for must be between 1 to 100 members'
+                    : null,
+            details: (value) =>
+                value.length <= 0 ? 'Details must be included' : null,
+        },
+    });
+
+    const handleSubmit = async (values) => {
+        console.log(values);
+    };
+
+    return (
+        <form
+            aria-label="Start a Party"
+            onSubmit={form.onSubmit((values) => handleSubmit(values))}
+        >
+            <TextInput
+                mt="md"
+                label="Party Name"
+                placeholder="My Cool Party"
+                {...form.getInputProps('name')}
+                withAsterisk
+            />
+            <DatePicker
+                mt="sm"
+                placeholder="Date of the event"
+                label="When to Meet"
+                {...form.getInputProps('date')}
+                withAsterisk
+            />
+            <NumberInput
+                mt="sm"
+                label="Looking For"
+                placeholder="# of Members"
+                min={1}
+                max={100}
+                {...form.getInputProps('lookingFor')}
+                withAsterisk
+            />
+            <Textarea
+                mt="sm"
+                placeholder="Details about your party"
+                label="Party Details"
+                {...form.getInputProps('details')}
+                minRows={4}
+                withAsterisk
+            />
+            <Button type="submit" mt="sm">
+                Submit
+            </Button>
+        </form>
+    );
 };
 
 export default CreatePartyForm;
--- client/src/tests/CreatePartyForm.test.js
+++ client/src/tests/CreatePartyForm.test.js
@@ -3,6 +3,7 @@ import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { AuthProvider } from '../context/AuthContext';
 import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 import CreatePartyForm from '../components/CreatePartyForm';
 
 const Wrapper = ({ children }) => {
@@ -13,8 +14,69 @@ const Wrapper = ({ children }) => {
     );
 };
 
+const seedInputs = {
+    name: 'Stardew Valley 4 Corners Playthrough',
+    lookingFor: 300,
+    details:
+        'Looking to start a new save with dedicated players. Going for 100% completion run.',
+};
+
 describe('Create party form', () => {
     it('renders a form', () => {
         render(<CreatePartyForm />, { wrapper: Wrapper });
+        expect(
+            screen.getByRole('form', {
+                name: /start a party/i,
+            })
+        ).toBeInTheDocument();
+    });
+
+    it('renders name, date, lookingFor, and details inputs', () => {
+        render(<CreatePartyForm />, { wrapper: Wrapper });
+
+        expect(
+            screen.getByPlaceholderText('My Cool Party')
+        ).toBeInTheDocument();
+
+        expect(
+            screen.getByPlaceholderText('Date of the event')
+        ).toBeInTheDocument();
+
+        expect(screen.getByPlaceholderText('# of Members')).toBeInTheDocument();
+
+        expect(
+            screen.getByPlaceholderText('Details about your party')
+        ).toBeInTheDocument();
+    });
+
+    it('renders a submit button', () => {
+        render(<CreatePartyForm />, { wrapper: Wrapper });
+        expect(screen.getByText('Submit')).toBeInTheDocument();
+    });
+
+    it('updates values on user input', () => {
+        render(<CreatePartyForm />, { wrapper: Wrapper });
+
+        const nameInput = screen.getByPlaceholderText('My Cool Party');
+        const detailsInput = screen.getByPlaceholderText(
+            'Details about your party'
+        );
+
+        userEvent.type(nameInput, seedInputs.name);
+        userEvent.type(detailsInput, seedInputs.details);
+
+        expect(nameInput).toHaveValue(seedInputs.name);
+        expect(detailsInput).toHaveValue(seedInputs.details);
+    });
+
+    it('limits looking for value to 100', () => {
+        render(<CreatePartyForm />, { wrapper: Wrapper });
+
+        const lookingForInput = screen.getByPlaceholderText('# of Members');
+
+        userEvent.type(lookingForInput, '300');
+        userEvent.tab();
+
+        expect(lookingForInput).toHaveValue('100');
     });
 });
