e60364a9533dcdcbd85118d4bdb7b41d609370ed
spencercoleman <spencer@edc.vc>
2022-08-23 00:48:39 -0700

    Handle new party submission and creation; Implement server-side validation checks

--- client/src/components/CreatePartyForm.js
+++ client/src/components/CreatePartyForm.js
@@ -1,8 +1,15 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useForm } from '@mantine/form';
 import { DatePicker } from '@mantine/dates';
-import { NumberInput, TextInput, Textarea, Button } from '@mantine/core';
+import { NumberInput, TextInput, Textarea, Button, Text } from '@mantine/core';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 const CreatePartyForm = () => {
+    const { user } = useAuthContext();
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+
     const form = useForm({
         initialValues: {
             name: '',
@@ -14,8 +21,7 @@ const CreatePartyForm = () => {
         validate: {
             name: (value) =>
                 value.length < 2 ? 'Name must have at least 2 letters' : null,
-            date: (value) =>
-                value.length <= 0 ? 'Date must be selected' : null,
+            date: (value) => (!value ? 'Date must be selected' : null),
             lookingFor: (value) =>
                 value < 1 || value > 100
                     ? 'Looking for must be between 1 to 100 members'
@@ -26,7 +32,30 @@ const CreatePartyForm = () => {
     });
 
     const handleSubmit = async (values) => {
-        console.log(values);
+        if (!user) {
+            setError('You must be logged in');
+            return;
+        }
+
+        const response = await fetch('/api/parties', {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${user.token}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(values),
+        });
+
+        const json = await response.json();
+
+        if (!response.ok) {
+            setError(json.error);
+        }
+
+        if (response.ok) {
+            setError(null);
+            navigate(`/parties/${json._id}`);
+        }
     };
 
     return (
@@ -65,6 +94,13 @@ const CreatePartyForm = () => {
                 minRows={4}
                 withAsterisk
             />
+
+            {error && (
+                <Text mt="sm" color="red">
+                    {error}
+                </Text>
+            )}
+
             <Button type="submit" mt="sm">
                 Submit
             </Button>
--- server/controllers/partyController.js
+++ server/controllers/partyController.js
@@ -3,202 +3,214 @@ const Party = require('../models/partyModel');
 const User = require('../models/userModel');
 
 const getParties = async (req, res) => {
-	try {
-		const parties = await Party.find().sort({ createdAt: -1 });
-		res.status(200).json(parties);
-	} catch (error) {
-		res.status(400).json({ error: error.message });
-	}
+    try {
+        const parties = await Party.find().sort({ createdAt: -1 });
+        res.status(200).json(parties);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 };
 
 const getParty = async (req, res) => {
-	const { id } = req.params;
+    const { id } = req.params;
 
-	// Check if ID is valid before query
-	if (!mongoose.Types.ObjectId.isValid(id)) {
-		return res.status(404).json({ error: 'Party not found' });
-	}
+    // Check if ID is valid before query
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'Party not found' });
+    }
 
-	// Get the party using the ID and populate leader, members, and messages data
-	const party = await Party.findById(id)
-		.populate({
-			path: 'leader',
-			select: 'username avatar -_id',
-		})
-		.populate({ path: 'members', select: 'username avatar -_id' })
-		.populate('messages');
+    // Get the party using the ID and populate leader, members, and messages data
+    const party = await Party.findById(id)
+        .populate({
+            path: 'leader',
+            select: 'username avatar -_id',
+        })
+        .populate({ path: 'members', select: 'username avatar -_id' })
+        .populate('messages');
 
-	if (!party) {
-		return res.status(404).json({ error: 'Party not found' });
-	}
+    if (!party) {
+        return res.status(404).json({ error: 'Party not found' });
+    }
 
-	res.status(200).json(party);
+    res.status(200).json(party);
 };
 
 const createParty = async (req, res) => {
-	const { date, details, lookingFor, memberCount, members, name } = req.body;
-	const userId = req.user._id;
+    const { date, details, lookingFor, name } = req.body;
+    const userId = req.user._id;
 
-	// TODO: Add validation checks
+    if (!date || !details || !lookingFor || !name) {
+        return res.status(400).json({ error: 'All fields must be filled' });
+    }
 
-	try {
-		// Add messages as empty array
-		const messages = [];
-		const party = await Party.create({
-			date,
-			details,
-			leader: userId,
-			lookingFor,
-			memberCount,
-			members,
-			messages,
-			name,
-		});
+    if (name.length === 0) {
+        return res.status(400).json({ error: 'Name must be provided' });
+    }
 
-		res.status(200).json(party);
-	} catch (error) {
-		res.status(400).json({ error: error.message });
-	}
+    if (details.length === 0) {
+        return res.status(400).json({ error: 'Details must be provided' });
+    }
+
+    if (lookingFor <= 0 || lookingFor > 100) {
+        return res
+            .status(400)
+            .json({ error: 'Members must be between 1 and 100' });
+    }
+
+    try {
+        const party = await Party.create({
+            date,
+            details,
+            leader: userId,
+            lookingFor,
+            members: [userId],
+            name,
+        });
+
+        res.status(200).json(party);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 };
 
 const updateParty = async (req, res) => {
-	const { id } = req.params;
+    const { id } = req.params;
 
-	// TODO: Add validation checks
+    // TODO: Add validation checks
 
-	// Check if ID is valid before query
-	if (!mongoose.Types.ObjectId.isValid(id)) {
-		return res.status(404).json({ error: 'Party not found' });
-	}
+    // Check if ID is valid before query
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'Party not found' });
+    }
 
-	const party = await Party.findOneAndUpdate(
-		{ _id: id },
-		{
-			...req.body,
-		},
-		{ new: true }
-	);
+    const party = await Party.findOneAndUpdate(
+        { _id: id },
+        {
+            ...req.body,
+        },
+        { new: true }
+    );
 
-	if (!party) {
-		return res.status(404).json({ error: 'Party not found' });
-	}
+    if (!party) {
+        return res.status(404).json({ error: 'Party not found' });
+    }
 
-	res.status(200).json(party);
+    res.status(200).json(party);
 };
 
 const deleteParty = async (req, res) => {
-	const { id } = req.params;
-	const userId = req.user._id;
+    const { id } = req.params;
+    const userId = req.user._id;
 
-	// Check if ID is valid before query
-	if (!mongoose.Types.ObjectId.isValid(id)) {
-		return res.status(404).json({ error: 'Party not found' });
-	}
+    // Check if ID is valid before query
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'Party not found' });
+    }
 
-	const party = await Party.findOneAndDelete({ _id: id });
+    const party = await Party.findOneAndDelete({ _id: id });
 
-	if (!party) {
-		return res.status(404).json({ error: 'Party not found' });
-	}
+    if (!party) {
+        return res.status(404).json({ error: 'Party not found' });
+    }
 
-	res.status(200).json(party);
+    res.status(200).json(party);
 };
 
 const joinParty = async (req, res) => {
-	const { id } = req.params;
-	const userId = req.user._id;
+    const { id } = req.params;
+    const userId = req.user._id;
 
-	// Check if ID is valid before query
-	if (!mongoose.Types.ObjectId.isValid(id)) {
-		return res.status(404).json({ error: 'Party not found' });
-	}
+    // Check if ID is valid before query
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'Party not found' });
+    }
 
-	const party = await Party.findById({ _id: id });
+    const party = await Party.findById({ _id: id });
 
-	if (!party) {
-		return res.status(404).json({ error: 'Party not found' });
-	}
+    if (!party) {
+        return res.status(404).json({ error: 'Party not found' });
+    }
 
-	// Check if party is full by checking member length vs lookingFor amount - Subtract one for leader
-	if (party.lookingFor - (party.members.length - 1) <= 0) {
-		return res.status(500).json({ error: 'Could not not join party' });
-	}
+    // Check if party is full by checking member length vs lookingFor amount - Subtract one for leader
+    if (party.lookingFor - (party.members.length - 1) <= 0) {
+        return res.status(500).json({ error: 'Could not not join party' });
+    }
 
-	// Add user to party members
-	const updated = await Party.findByIdAndUpdate(
-		{ _id: id },
-		{
-			$addToSet: {
-				members: userId,
-			},
-		},
-		{
-			new: true,
-		}
-	);
+    // Add user to party members
+    const updated = await Party.findByIdAndUpdate(
+        { _id: id },
+        {
+            $addToSet: {
+                members: userId,
+            },
+        },
+        {
+            new: true,
+        }
+    );
 
-	// Add party to user party list
-	await User.findByIdAndUpdate(
-		{
-			_id: userId,
-		},
-		{
-			$addToSet: {
-				parties: id,
-			},
-		}
-	);
+    // Add party to user party list
+    await User.findByIdAndUpdate(
+        {
+            _id: userId,
+        },
+        {
+            $addToSet: {
+                parties: id,
+            },
+        }
+    );
 
-	res.status(200).json(updated);
+    res.status(200).json(updated);
 };
 
 const leaveParty = async (req, res) => {
-	const { id } = req.params;
-	const userId = req.user._id;
+    const { id } = req.params;
+    const userId = req.user._id;
 
-	// Check if ID is valid before query
-	if (!mongoose.Types.ObjectId.isValid(id)) {
-		return res.status(404).json({ error: 'Party not found' });
-	}
+    // Check if ID is valid before query
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'Party not found' });
+    }
 
-	// Remove user from party members
-	const party = await Party.findByIdAndUpdate(
-		{ _id: id },
-		{
-			$pull: {
-				members: userId,
-			},
-		},
-		{
-			new: true,
-		}
-	);
+    // Remove user from party members
+    const party = await Party.findByIdAndUpdate(
+        { _id: id },
+        {
+            $pull: {
+                members: userId,
+            },
+        },
+        {
+            new: true,
+        }
+    );
 
-	if (!party) {
-		return res.status(404).json({ error: 'Party not found' });
-	}
+    if (!party) {
+        return res.status(404).json({ error: 'Party not found' });
+    }
 
-	// Remove party from user list
-	await User.findByIdAndUpdate(
-		{
-			_id: userId,
-		},
-		{
-			$pull: {
-				parties: id,
-			},
-		}
-	);
+    // Remove party from user list
+    await User.findByIdAndUpdate(
+        {
+            _id: userId,
+        },
+        {
+            $pull: {
+                parties: id,
+            },
+        }
+    );
 
-	res.status(200).json(party);
+    res.status(200).json(party);
 };
 
 module.exports = {
-	getParties,
-	getParty,
-	createParty,
-	updateParty,
-	deleteParty,
-	joinParty,
-	leaveParty,
+    getParties,
+    getParty,
+    createParty,
+    updateParty,
+    deleteParty,
+    joinParty,
+    leaveParty,
 };
