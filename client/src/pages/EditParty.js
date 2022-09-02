ec74ee708797c1cb9df66aebace521ec09ddd49f
spencercoleman <spencer@edc.vc>
2022-09-01 23:44:38 -0700

    Set up party fetch on EditParty page

--- client/src/pages/EditParty.js
+++ client/src/pages/EditParty.js
@@ -1,5 +1,37 @@
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
 const EditParty = () => {
-    return <div>Edit Party Page</div>;
+    const { id } = useParams();
+    const [party, setParty] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const getParty = async () => {
+            const response = await fetch(`/api/parties/${id}`);
+            const json = await response.json();
+
+            if (!response.ok) {
+                setError(json.error);
+            }
+
+            if (response.ok) {
+                setParty(json);
+            }
+        };
+
+        getParty();
+    }, [id]);
+
+    if (party) {
+        return <div>{party.name}</div>;
+    }
+
+    if (error) {
+        return <div>{error}</div>;
+    }
+
+    return <div>Loading</div>;
 };
 
 export default EditParty;
