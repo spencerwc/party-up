c7f48a8d4abd5fb32d6d4ccb7ee03074f1dba3e8
spencercoleman <spencer@edc.vc>
2022-09-05 12:56:20 -0700

    Set up FilterForm component

---
+++ client/src/components/general/FilterForm.js
@@ -0,0 +1,21 @@
+import { TextInput } from '@mantine/core';
+import { IconSearch } from '@tabler/icons';
+
+const FilterForm = ({ placeholder, filter }) => {
+    const handleChange = (e) => {
+        filter(e.target.value);
+    };
+
+    return (
+        <TextInput
+            mt="md"
+            icon={<IconSearch size={18} stroke={1.5} />}
+            size="md"
+            placeholder={placeholder}
+            rightSectionWidth={42}
+            onChange={handleChange}
+        />
+    );
+};
+
+export default FilterForm;
