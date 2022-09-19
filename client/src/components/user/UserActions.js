ac7b2fe763ea26967fe152d44f34d988f37181bb
spencercoleman <spencer@edc.vc>
2022-09-18 23:20:36 -0700

    Set up basic add and remove friend functions on client

--- client/src/components/user/UserActions.js
+++ client/src/components/user/UserActions.js
@@ -1,14 +1,27 @@
-import { Group } from '@mantine/core';
+import { ActionIcon, Group } from '@mantine/core';
 import { IconMessage, IconUserPlus, IconUserMinus } from '@tabler/icons';
 
-const UserActions = () => {
-    return (
-        <Group m="md" mt={20}>
-            <IconMessage size={20} stroke={1.5} />
-            <IconUserPlus size={20} stroke={1.5} />
-            <IconUserMinus size={20} stroke={1.5} />
-        </Group>
-    );
+const UserActions = ({ isFriend, addFriend }) => {
+    if (isFriend) {
+        return (
+            <Group m="md" mt={20} spacing={4}>
+                <ActionIcon radius="md">
+                    <IconMessage size={20} stroke={1.5} />
+                </ActionIcon>
+                <ActionIcon radius="md">
+                    <IconUserMinus size={20} stroke={1.5} />
+                </ActionIcon>
+            </Group>
+        );
+    } else {
+        return (
+            <Group m="md" mt={20}>
+                <ActionIcon onClick={addFriend} radius="md">
+                    <IconUserPlus size={20} stroke={1.5} />
+                </ActionIcon>
+            </Group>
+        );
+    }
 };
 
 export default UserActions;
--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { useFetch } from '../hooks/useFetch';
 import { useParams } from 'react-router-dom';
 import { useAuthContext } from '../hooks/useAuthContext';
@@ -11,13 +12,41 @@ const User = ({ setIsRegistering }) => {
     const { username } = useParams();
     const { user } = useAuthContext();
     const { data: userData, error } = useFetch(`/api/users/${username}`);
+    const [isFriend, setIsFriend] = useState(false);
+
+    const addFriend = async () => {
+        // Connect fetch request here
+        setIsFriend(true);
+    };
+
+    const removeFriend = async () => {
+        // Connect fetch request here
+        setIsFriend(false);
+    };
+
+    useEffect(() => {
+        const checkIfFriend = () => {
+            const index = userData.friends.findIndex(
+                (friend) => friend.username === user.username
+            );
+
+            if (index === 1) {
+                setIsFriend(false);
+            }
+            setIsFriend(true);
+        };
+
+        if (userData) {
+            checkIfFriend();
+        }
+    }, [userData, user]);
 
     if (userData) {
         return (
             <Stack>
                 <Group spacing="xs" noWrap position="apart" align="flex-start">
                     <UserDetails user={userData} />
-                    <UserActions userData={userData} />
+                    <UserActions isFriend={isFriend} addFriend={addFriend} />
                 </Group>
 
                 {userData.friends.length > 0 && (
