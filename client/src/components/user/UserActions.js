b3177a86f3a90030599227a6765c9bee4ac88caa
spencercoleman <spencer@edc.vc>
2022-09-20 00:22:57 -0700

    Handle friend request state cancelling and state change in client

--- client/src/components/user/UserActions.js
+++ client/src/components/user/UserActions.js
@@ -1,30 +1,78 @@
-import { ActionIcon, Group } from '@mantine/core';
-import { IconMessage, IconUserPlus, IconUserMinus } from '@tabler/icons';
+import { Box, ActionIcon, Group, Tooltip } from '@mantine/core';
+import {
+    IconMessage,
+    IconUserPlus,
+    IconUserMinus,
+    IconUserX,
+} from '@tabler/icons';
 
-const UserActions = ({ isFriend, addFriend, setIsConfirmingRemove }) => {
+const UserActions = ({
+    isFriend,
+    sendFriendRequest,
+    hasRequestedFriend,
+    setIsConfirmingRemove,
+    setIsConfirmingCancel,
+}) => {
+    const handleRequest = () => {
+        if (!hasRequestedFriend) {
+            sendFriendRequest();
+        }
+    };
+
+    if (hasRequestedFriend) {
+        return (
+            <Box m="md" mt={20}>
+                <Tooltip label="Cancel Request">
+                    <ActionIcon
+                        radius="md"
+                        variant="filled"
+                        onClick={() => setIsConfirmingCancel(true)}
+                    >
+                        <IconUserX size={20} stroke={1.5} />
+                    </ActionIcon>
+                </Tooltip>
+            </Box>
+        );
+    }
+
     if (isFriend) {
         return (
-            <Group m="md" mt={20} spacing={4}>
-                <ActionIcon radius="md">
-                    <IconMessage size={20} stroke={1.5} />
-                </ActionIcon>
-                <ActionIcon
-                    radius="md"
-                    onClick={() => setIsConfirmingRemove(true)}
-                >
-                    <IconUserMinus size={20} stroke={1.5} />
-                </ActionIcon>
-            </Group>
-        );
-    } else {
-        return (
-            <Group m="md" mt={20}>
-                <ActionIcon onClick={addFriend} radius="md">
-                    <IconUserPlus size={20} stroke={1.5} />
-                </ActionIcon>
+            <Group m="md" mt={20} spacing={6}>
+                <Tooltip label="Message">
+                    <ActionIcon radius="md" variant="filled" color="">
+                        <IconMessage size={20} stroke={1.5} />
+                    </ActionIcon>
+                </Tooltip>
+
+                <Tooltip label="Remove Friend">
+                    <ActionIcon
+                        radius="md"
+                        variant="filled"
+                        color="red.8"
+                        onClick={() => setIsConfirmingRemove(true)}
+                    >
+                        <IconUserMinus size={20} stroke={1.5} />
+                    </ActionIcon>
+                </Tooltip>
             </Group>
         );
     }
+
+    return (
+        <Group m="md" mt={20}>
+            <Tooltip label="Add friend">
+                <ActionIcon
+                    radius="md"
+                    variant="filled"
+                    color=""
+                    onClick={handleRequest}
+                    disabled={hasRequestedFriend}
+                >
+                    <IconUserPlus size={20} stroke={1.5} />
+                </ActionIcon>
+            </Tooltip>
+        </Group>
+    );
 };
 
 export default UserActions;
--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -2,49 +2,70 @@ import { useState, useEffect } from 'react';
 import { useFetch } from '../hooks/useFetch';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useAuthContext } from '../hooks/useAuthContext';
-import { Stack, Group, Text } from '@mantine/core';
+import { Stack, Group, Text, Title } from '@mantine/core';
 import MinimalLoader from '../components/general/MinimalLoader';
 import UserDetails from '../components/user/UserDetails';
 import UserActions from '../components/user/UserActions';
 import UserCardList from '../components/users/UserCardList';
 import ConfirmationModal from '../components/general/ConfirmationModal';
 
-const User = ({ setIsRegistering }) => {
+const User = () => {
     const { username } = useParams();
     const { user } = useAuthContext();
     const navigate = useNavigate();
     const { data: userData, error } = useFetch(`/api/users/${username}`);
+    const [friendRequests, setFriendRequests] = useState(null);
     const [isFriend, setIsFriend] = useState(false);
+    const [hasRequestedFriend, setHasRequestedFriend] = useState(false);
+    const [isConfirmingCancel, setIsConfirmingCancel] = useState(false);
     const [isConfirmingRemove, setIsConfirmingRemove] = useState(false);
-    const [friendError, setFriendError] = useState(null);
     const [isPending, setIsPending] = useState(false);
+    const [friendError, setFriendError] = useState(null);
 
-    const addFriend = async () => {
-        // Connect fetch request here
-        if (!user) {
-            setIsRegistering(true);
-        } else {
-            setIsPending(true);
-
-            const response = await fetch(
-                `/api/users/${userData.username}/friends/add`,
-                {
-                    method: 'PATCH',
-                    headers: {
-                        Authorization: `Bearer ${user.token}`,
-                    },
-                }
-            );
-
-            if (!response.ok) {
-                const json = await response.json();
-                setFriendError(json.error);
-                setIsPending(false);
+    const sendFriendRequest = async () => {
+        const response = await fetch(
+            `/api/users/${userData.username}/friends/request`,
+            {
+                method: 'PATCH',
+                headers: {
+                    Authorization: `Bearer ${user.token}`,
+                },
             }
+        );
 
-            if (response.ok) {
-                navigate(0);
+        if (!response.ok) {
+            const json = await response.json();
+            setFriendError(json.error);
+            setIsPending(false);
+        }
+
+        if (response.ok) {
+            // Update requested state and Notify that the request was sent
+            setHasRequestedFriend(true);
+        }
+    };
+
+    const cancelFriendRequest = async () => {
+        const response = await fetch(
+            `/api/users/${userData.username}/friends/request/cancel`,
+            {
+                method: 'PATCH',
+                headers: {
+                    Authorization: `Bearer ${user.token}`,
+                },
             }
+        );
+
+        if (!response.ok) {
+            const json = await response.json();
+            setFriendError(json.error);
+            setIsConfirmingCancel(false);
+        }
+
+        if (response.ok) {
+            // Update requested state and Notify that the request was canceled
+            setHasRequestedFriend(false);
+            setIsConfirmingCancel(false);
         }
     };
 
@@ -73,6 +94,29 @@ const User = ({ setIsRegistering }) => {
     };
 
     useEffect(() => {
+        const getFriendRequests = async () => {
+            const response = await fetch(
+                `/api/users/${user.username}/friends/requests`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${user.token}`,
+                    },
+                }
+            );
+
+            const json = await response.json();
+
+            if (response.ok) {
+                setFriendRequests(json);
+            }
+        };
+
+        if (user) {
+            getFriendRequests();
+        }
+    }, [user]);
+
+    useEffect(() => {
         const checkIfFriend = () => {
             const index = userData.friends.findIndex(
                 (friend) => friend.username === user.username
@@ -86,11 +130,39 @@ const User = ({ setIsRegistering }) => {
         if (user && userData) {
             checkIfFriend();
         }
-    }, [userData, user]);
+    }, [userData, user, friendRequests]);
+
+    // Check user friend requests for existing request
+    useEffect(() => {
+        const checkIfRequested = async () => {
+            const index = friendRequests.friendRequestsSent.findIndex(
+                (user) => user.username === userData.username
+            );
+
+            if (index !== -1) {
+                setHasRequestedFriend(true);
+            }
+        };
+
+        if (user && userData && friendRequests) {
+            checkIfRequested();
+        }
+    }, [user, userData, friendRequests]);
 
     if (userData) {
         return (
             <Stack>
+                {/* Confirmation for canceling a request */}
+                <ConfirmationModal
+                    isConfirming={isConfirmingCancel}
+                    setIsConfirming={setIsConfirmingCancel}
+                    title="Cancel friend request?"
+                    body={`Your request to ${userData.username} will be withdrawn.`}
+                    action={cancelFriendRequest}
+                    isPending={isPending}
+                />
+
+                {/* Confirmation for removing a friend */}
                 <ConfirmationModal
                     isConfirming={isConfirmingRemove}
                     setIsConfirming={setIsConfirmingRemove}
@@ -103,22 +175,32 @@ const User = ({ setIsRegistering }) => {
                 <Group spacing="xs" noWrap position="apart" align="flex-start">
                     <UserDetails user={userData} />
 
-                    {user.username !== username && (
+                    {user && user.username !== username && friendRequests && (
                         <UserActions
                             isFriend={isFriend}
-                            addFriend={addFriend}
+                            sendFriendRequest={sendFriendRequest}
+                            hasRequestedFriend={hasRequestedFriend}
                             setIsConfirmingRemove={setIsConfirmingRemove}
-                            setIsRegistering={setIsRegistering}
+                            setIsConfirmingCancel={setIsConfirmingCancel}
                         />
                     )}
                 </Group>
 
-                {userData.friends.length > 0 && (
+                {userData.friends.length > 0 ? (
                     <UserCardList
                         title="Friends"
                         seeAllLink="friends"
                         users={userData.friends}
                     />
+                ) : (
+                    <Stack px="md" spacing="xs">
+                        <Title order={2} size={20}>
+                            Friends
+                        </Title>
+                        <Text color="dimmed">
+                            {userData.username} hasn't added any friends yet.
+                        </Text>
+                    </Stack>
                 )}
             </Stack>
         );
