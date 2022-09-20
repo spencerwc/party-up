73e64f429dddd1ef5e52adc486e3e06d46d84fdb
spencercoleman <spencer@edc.vc>
2022-09-20 16:33:30 -0700

    Populate request data and style request list

---
+++ client/src/components/general/Request.js
@@ -0,0 +1,86 @@
+import { Link } from 'react-router-dom';
+import {
+    Anchor,
+    Avatar,
+    ActionIcon,
+    Text,
+    Tooltip,
+    Group,
+    createStyles,
+} from '@mantine/core';
+import { IconCheck, IconX } from '@tabler/icons';
+
+const useStyles = createStyles((theme) => ({
+    request: {
+        borderTop: `1px solid ${
+            theme.colorScheme === 'dark'
+                ? theme.colors.dark[6]
+                : theme.colors.gray[2]
+        }`,
+
+        '&:first-of-type': {
+            borderTop: '0px',
+        },
+
+        [`@media (min-width: ${theme.breakpoints.md}px)`]: {
+            transition: 'all 0.25s',
+
+            '&:hover': {
+                boxShadow: 'rgba(0, 0, 0, 0.04) 0px 3px 5px',
+
+                '&:last-child': {
+                    boxShadow: 'none',
+                },
+            },
+        },
+    },
+}));
+
+const Request = ({ type, request }) => {
+    const { classes } = useStyles();
+
+    return (
+        <Group
+            className={classes.request}
+            p="sm"
+            pr="md"
+            position="apart"
+            noWrap
+        >
+            <Group spacing="xs" noWrap>
+                <Anchor component={Link} to={`/users/${request.username}`}>
+                    <Avatar src={request.avatar} size={60} radius={120} />
+                </Anchor>
+                <Anchor
+                    component={Link}
+                    to={`/users/${request.username}`}
+                    variant="text"
+                >
+                    <Text weight={500}>{request.username}</Text>
+                </Anchor>
+            </Group>
+            {type === 'sent' ? (
+                <Tooltip label="Cancel Request">
+                    <ActionIcon>
+                        <IconX size={18} stroke={1.5} />
+                    </ActionIcon>
+                </Tooltip>
+            ) : (
+                <Group spacing={6}>
+                    <Tooltip label="Accept request">
+                        <ActionIcon>
+                            <IconCheck size={18} stroke={1.5} />
+                        </ActionIcon>
+                    </Tooltip>
+                    <Tooltip label="Deny Request">
+                        <ActionIcon>
+                            <IconX size={18} stroke={1.5} />
+                        </ActionIcon>
+                    </Tooltip>
+                </Group>
+            )}
+        </Group>
+    );
+};
+
+export default Request;
--- client/src/components/general/RequestList.js
+++ client/src/components/general/RequestList.js
@@ -1,6 +1,21 @@
-import { Stack, Title, Text } from '@mantine/core';
+import { Stack, Title, Text, Paper, createStyles } from '@mantine/core';
+import Request from './Request';
+
+const useStyles = createStyles((theme) => ({
+    requests: {
+        boxShadow: 'rgba(0, 0, 0, 0.04) 0px 3px 5px',
+
+        [`@media (min-width: ${theme.breakpoints.md}px)`]: {
+            marginRight: theme.spacing.md,
+            marginLeft: theme.spacing.md,
+            borderRadius: theme.radius.lg,
+        },
+    },
+}));
 
 const RequestList = ({ title, type, requests }) => {
+    const { classes } = useStyles();
+
     return (
         <Stack>
             <Title mx="md" mt="md" order={2} size={17} weight={500}>
@@ -8,11 +23,11 @@ const RequestList = ({ title, type, requests }) => {
             </Title>
 
             {requests.length > 0 ? (
-                requests.map((request, index) => (
-                    <Text key={index} mx="md">
-                        {request.username}
-                    </Text>
-                ))
+                <Paper className={classes.requests}>
+                    {requests.map((request, index) => (
+                        <Request key={index} type={type} request={request} />
+                    ))}
+                </Paper>
             ) : (
                 <Text mx="md" color="dimmed">
                     You have no requests.
