40581f34fb1c959629784dc55d963f348e06d42f
spencercoleman <spencer@edc.vc>
2022-09-21 22:37:29 -0700

    Implement mantine notifications and provider

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
+import { NotificationsProvider } from '@mantine/notifications';
 import { ThemeProvider } from './context/ThemeContext';
 import App from './App';
 
@@ -9,11 +10,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <ThemeProvider>
-            <AuthProvider>
-                <BrowserRouter>
-                    <App />
-                </BrowserRouter>
-            </AuthProvider>
+            <NotificationsProvider>
+                <AuthProvider>
+                    <BrowserRouter>
+                        <App />
+                    </BrowserRouter>
+                </AuthProvider>
+            </NotificationsProvider>
         </ThemeProvider>
     </React.StrictMode>
 );
