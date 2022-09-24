79cecdd80d9f2f27528be92e79f42cbbf8e39e74
spencercoleman <spencer@edc.vc>
2022-09-23 18:46:14 -0700

    Implement date range and conditional filtering on parties list

--- client/src/pages/Parties.js
+++ client/src/pages/Parties.js
@@ -1,12 +1,67 @@
-import { Box, Title } from '@mantine/core';
+import { Box, Title, Group, Select, Checkbox, MediaQuery } from '@mantine/core';
 import { useFetch } from '../hooks/useFetch';
 import MinimalLoader from '../components/general/MinimalLoader';
 import PartiesList from '../components/parties/PartiesList';
+import dayjs from 'dayjs';
+import isToday from 'dayjs/plugin/isToday';
+import isBetween from 'dayjs/plugin/isBetween';
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
+import { useState } from 'react';
+
+dayjs.extend(isToday);
+dayjs.extend(isBetween);
+dayjs.extend(isSameOrAfter);
 
 const Parties = () => {
     const { data: parties, error } = useFetch('/api/parties');
+    const [range, setRange] = useState('Today');
+    const [hideFilled, setHideFilled] = useState(false);
+    const [hidePast, setHidePast] = useState(false);
+
+    const getPartiesInRange = () => {
+        switch (range) {
+            case 'Today':
+                return parties.filter((party) => dayjs(party.date).isToday());
+            case 'This Week':
+                return parties.filter((party) =>
+                    dayjs(party.date).isBetween(
+                        dayjs().subtract(1, 'day'),
+                        dayjs().day(6),
+                        '(]'
+                    )
+                );
+            case 'This Month':
+                return parties.filter(
+                    (party) =>
+                        dayjs(party.date).isSameOrAfter(dayjs(), 'day') &&
+                        dayjs(party.date).month() === dayjs().month()
+                );
+            default:
+                return parties;
+        }
+    };
+
+    const getFilteredParties = () => {
+        let filteredParties = getPartiesInRange(parties);
+
+        if (hideFilled) {
+            filteredParties = filteredParties.filter(
+                (party) => party.members.length - 1 !== party.lookingFor
+            );
+        }
+
+        if (hidePast) {
+            filteredParties = filteredParties.filter((party) =>
+                dayjs(party.date).isSameOrAfter(dayjs(), 'day')
+            );
+        }
+
+        return filteredParties;
+    };
 
     if (parties) {
+        const filtered = getFilteredParties();
+
         return (
             <Box
                 sx={(theme) => ({
@@ -32,7 +87,47 @@ const Parties = () => {
                 >
                     Parties
                 </Title>
-                <PartiesList parties={parties} />
+
+                <Group
+                    mb="md"
+                    sx={(theme) => ({
+                        [`@media (max-width: ${theme.breakpoints.md}px)`]: {
+                            marginLeft: theme.spacing.md,
+                            marginRight: theme.spacing.md,
+                        },
+                    })}
+                    noWrap
+                >
+                    <Select
+                        value={range}
+                        onChange={setRange}
+                        data={['Today', 'This Week', 'This Month', 'All Time']}
+                        radius="md"
+                        size="sm"
+                        sx={{
+                            maxWidth: 140,
+                        }}
+                        variant="filled"
+                    />
+                    <Checkbox
+                        label="Hide filled"
+                        checked={hideFilled}
+                        onChange={(event) =>
+                            setHideFilled(event.currentTarget.checked)
+                        }
+                        size="xs"
+                    />
+                    <Checkbox
+                        label="Hide past"
+                        checked={hidePast}
+                        onChange={(event) =>
+                            setHidePast(event.currentTarget.checked)
+                        }
+                        size="xs"
+                    />
+                </Group>
+
+                <PartiesList parties={filtered} />
             </Box>
         );
     }
